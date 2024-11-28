ea4201d6d4602a621bcb242ac43a875e8cc70228
Your Name <you@example.com>
2024-11-27 23:24:54 -0400

    actualizacion historial de rutas

--- lib/model/RutaCargaPedido.dart
+++ lib/model/RutaCargaPedido.dart
@@ -1,4 +1,5 @@
 // lib/model/ruta_carga_pedido.dart
+import 'package:latlong2/latlong.dart' as latLngLib;
 
 class RutaCargaPedido {
   final int? id; // El ID es opcional ya que solo es necesario para rutas de carga de pedido existentes
@@ -8,7 +9,7 @@ class RutaCargaPedido {
   final int orden;
   final String estado;
   final double distancia;
-
+ final List<latLngLib.LatLng> points;
   // Constructor
   RutaCargaPedido({
     this.id,
@@ -18,20 +19,28 @@ class RutaCargaPedido {
     required this.orden,
     required this.estado,
     required this.distancia,
+     required this.points,
   });
 
   // Método para crear un objeto RutaCargaPedido a partir de JSON
-  factory RutaCargaPedido.fromJson(Map<String, dynamic> json) {
-    return RutaCargaPedido(
-      id: json['id'],
-      idCargaPedido: json['id_carga_pedido'],
-      idRutaPedido: json['id_ruta_pedido'],
-      idTransporte: json['id_transporte'],
-      orden: json['orden'],
-      estado: json['estado'],
-      distancia: (json['distancia'] as num).toDouble(),
-    );
-  }
+ factory RutaCargaPedido.fromJson(Map<String, dynamic> json) {
+  return RutaCargaPedido(
+    id: json['id'],
+    idCargaPedido: json['id_carga_pedido'],
+    idRutaPedido: json['id_ruta_pedido'],
+    idTransporte: json['id_transporte'],
+    orden: json['orden'],
+    estado: json['estado'],
+    distancia: double.tryParse(json['distancia'].toString()) ?? 0.0, // Conversión segura
+    points: json['points'] != null
+        ? (json['points'] as List<dynamic>)
+            .map((point) => latLngLib.LatLng(point['lat'], point['lon']))
+            .toList()
+        : [],
+  );
+}
+
+
 
   // Método para convertir un objeto RutaCargaPedido a JSON
   Map<String, dynamic> toJson() {
